2c7ba1e27c259282896e485b164c5b13bf1b5b56
Novout <novout@hotmail.com>
2021-01-24 02:45:53 -0300

    feat: mvp

--- src/main.js
+++ src/main.js
@@ -1,3 +1,92 @@
+const headers = new Headers({
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "*"
+});
+
+const enterAccount = async (e) => {
+  e.preventDefault();
+
+  const email = document.querySelector("#email");
+  const password = document.querySelector("#password");
+
+  await fetch("http://api-challenges3.ddns.net:8081/api/Auth/SignIn", {
+    method: 'POST',
+    mode: 'cors',
+    body: JSON.stringify({ email: email.value, password: password.value }),
+    headers
+  })
+  .then(res => {
+    if(document.querySelector("#response")) document.querySelector("#response").remove();
+
+    const response = document.createElement("p");
+    response.style.color = "black";
+    response.id = "response";
+
+    if(res.status === 400 || res.status === 403 ||res.status === 404) {
+      response.textContent = `Não foi possível logar :(`;
+    }
+    else {
+      setTimeout(() => {
+        toDashboardFunction();
+      }, 500);
+    }
+
+    const container = document.querySelector("#login");
+    container.insertAdjacentElement("afterbegin", response);
+  })
+}
+
+const registerAccount = async (e) => {
+  e.preventDefault();
+
+  validateRegister();
+
+  const email = document.querySelector("#login");
+  const password = document.querySelector("#password");
+
+  await fetch("http://api-challenges3.ddns.net:8081/api/Auth/SignUp", {
+    method: 'POST',
+    mode: 'cors',
+    body: JSON.stringify({ email: email.value, password: password.value }),
+    headers
+  })
+  .then(res => {
+    console.log(res);
+    if(document.querySelector("#response")) document.querySelector("#response").remove();
+
+    const response = document.createElement("p");
+    response.style.color = "white";
+    response.id = "response";
+
+    if(res.status === 400 || res.status === 404) {
+      response.textContent = `Não foi possível cadastrar :(`;
+    }
+    else if(res.status === 409) {
+      response.textContent = `Este email já está cadastrado!`;
+    } 
+    else {
+      setTimeout(() => {
+        toLoginFunction();
+      }, 500);
+    }
+
+    const container = document.querySelector("#register");
+    container.insertAdjacentElement("afterbegin", response);
+  })
+}
+
+const validateRegister = () => {
+  const email = document.querySelector("#login");
+  const password = document.querySelector("#password");
+
+  if(!email.value || email.value === undefined) email.classList.add("form--error");
+  else if(email.classList.contains("form--error")) email.classList.remove("form--error");
+
+  if(!password.value || password.value.length < 4) password.classList.add("form--error");
+  else if(password.classList.contains("form--error")) password.classList.remove("form--error");
+}
+
 const toRegisterFunction = () => {
   const background = document.querySelector("#background");
   background.style.height = "100vh";
@@ -58,10 +147,12 @@ const createRegister = () => {
   const login = document.createElement("input");
   login.placeholder = "Digite o seu email..."
   login.type = "email";
+  login.id = "login";
 
   const password = document.createElement("input");
   password.type = "password";
   password.placeholder = "Digite a sua senha...";
+  password.id = "password";
 
   const enter = document.createElement("section");
   enter.classList.add("enter--register");
@@ -70,11 +161,13 @@ const createRegister = () => {
   toLoginRedirect.textContent = "aqui";
   const toLogin = document.createElement("p");
   toLogin.innerHTML = "Já possui uma conta? Clique ";
+  toLogin.style.color = "white";
   toLogin.insertAdjacentElement("beforeend", toLoginRedirect);
   toLoginRedirect.addEventListener("click", toLoginFunction);
 
   const loginAccount = document.createElement("button");
   loginAccount.textContent = "Registrar";
+  loginAccount.addEventListener("click", registerAccount);
 
   enter.insertAdjacentElement("afterbegin", toLogin);
   enter.insertAdjacentElement("beforeend", loginAccount);
@@ -124,10 +217,12 @@ const createLogin = () => {
   const login = document.createElement("input");
   login.placeholder = "Digite o seu email..."
   login.type = "email";
+  login.id = "email";
 
   const password = document.createElement("input");
   password.type = "password";
   password.placeholder = "Digite a sua senha...";
+  password.id = "password";
 
   const enter = document.createElement("section");
   enter.classList.add("enter--login");
@@ -141,6 +236,7 @@ const createLogin = () => {
 
   const loginAccount = document.createElement("button");
   loginAccount.textContent = "Entrar";
+  loginAccount.addEventListener("click", enterAccount);
 
   enter.insertAdjacentElement("afterbegin", toRegister);
   enter.insertAdjacentElement("beforeend", loginAccount);
@@ -158,6 +254,17 @@ const createLogin = () => {
   app.appendChild(container);
 }
 
+const toDashboardFunction = () => {
+  const dashboard = document.querySelector("#dashboard");
+  dashboard.style.width = "3000px";
+  dashboard.style.height = "3000px";
+
+  setInterval(() => {
+    const title = document.querySelector("#hello");
+    title.style.opacity = "1";
+  }, 1000);
+}
+
 const start = () => {
   const app = document.querySelector("#app");
 
@@ -165,7 +272,26 @@ const start = () => {
   background.id = "background";
   background.classList.add("background");
 
+  const dashboard = document.createElement("main");
+  dashboard.id = "dashboard";
+  dashboard.classList.add("bg--dashboard");
+
+  const title = document.createElement("h1");
+  title.style.fontSize = "3rem";
+  title.style.fontFamily = "Poppins";
+  title.style.fontWeight = "700";
+  title.style.color = "#7342e2";
+  title.style.zIndex = "40";
+  title.style.opacity = "0";
+  title.style.transition = "all 0.5s ease";
+  title.classList.add("center");
+  title.id = "hello";
+  title.textContent = "Hello He4rt";
+
   app.appendChild(background);
+
+  dashboard.appendChild(title);
+  app.appendChild(dashboard);
 
   createLogin();
 }
